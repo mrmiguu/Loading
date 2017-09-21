1f77b89f1c4b405f9914d585c22ea3ca43560324
PǝRV <alexander.w.matulionis@gmail.com>
2017-09-21 11:09:41 -0700

    loader done

--- done.go
+++ done.go
@@ -1,18 +1,35 @@
 package done
 
+import "strings"
+
 func Yet(proc string) chan<- bool {
-  c := make(chan bool)
-  println(proc+".")
-  spaces := strings.Repeat(" ",len(proc))
-  go func() {
-    i := 1
-    for done := range c {
-      if done {
-        println(spaces+"!!!")
-        return
-      }
-      println(spaces+"...")
-    }
-  }()
-  return c
+	c := make(chan bool)
+	println(proc + ".")
+	space := "   "
+	spaces := strings.Repeat(" ", len(proc))
+	var left bool
+	go func() {
+		i := 1
+		for done := range c {
+			if done {
+				println(spaces + "!!!")
+				return
+			}
+			strs := strings.Split(space, "")
+			strs[i] = "."
+			dot := strings.Join(strs, "")
+			println(spaces + dot)
+			if !left && i == 2 {
+				left = true
+			} else if left && i == 0 {
+				left = false
+			}
+			if !left {
+				i++
+			} else {
+				i--
+			}
+		}
+	}()
+	return c
 }
