1809f15c1bd7ba7a68874c4d7344258e1872299d
PǝRV <alexander.w.matulionis@gmail.com>
2017-09-21 12:05:03 -0700

    load.Ing

--- loading.go
+++ loading.go
@@ -1,17 +1,31 @@
-package done
+package load
 
 import "strings"
+import "time"
 
-func Yet(proc string) chan<- bool {
+func Ing(proc string, d ...time.Duration) chan<- bool {
 	c := make(chan bool)
 	println(proc + ".")
 	space := "   "
 	spaces := strings.Repeat(" ", len(proc))
 	var left bool
 	go func() {
+		var t time.Ticker
+		if len(d) > 1 {
+			panic("too many arguments")
+		}
+		if len(d) > 0 {
+			t = *time.NewTicker(d[0])
+			go func() {
+				for range t.C {
+					c <- false
+				}
+			}()
+		}
 		i := 1
 		for done := range c {
 			if done {
+				t.Stop()
 				println(spaces + "!!!")
 				return
 			}
